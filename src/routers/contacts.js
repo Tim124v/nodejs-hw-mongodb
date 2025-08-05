04db826dc19db3cb1ad2dc46b5d11482b370fbd1
Timur Bakiiev <177857078+Tim124v@users.noreply.github.com>
2025-08-05 07:52:17 +0200

    hw- modol-4 vallidation start work (#5)

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { getContactsController, getContactByIdController, createContactController, updateContactController, deleteContactController } from '../controllers/contactsController.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = express.Router();
 
 router.get('/', ctrlWrapper(getContactsController));
-router.get('/:contactId', ctrlWrapper(getContactByIdController));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 router.post('/', ctrlWrapper(createContactController));
-router.patch('/:contactId', ctrlWrapper(updateContactController));
-router.delete('/:contactId', ctrlWrapper(deleteContactController));
+router.patch('/:contactId', isValidId, ctrlWrapper(updateContactController));
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
 export default router;
